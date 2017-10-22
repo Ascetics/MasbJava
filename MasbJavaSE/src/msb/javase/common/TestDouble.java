18b225e4c9225d6e7a34685fb7b685bca7fc119e
mingkun.ling <mingkun.ling@qq.com>
2017-10-22 22:29:45 +0800

    马士兵J2SE，第06章_常用类，08_基础数据类型包装类.avi

---
+++ MasbJavaSE/src/msb/javase/common/TestDouble.java
@@ -0,0 +1,38 @@
+/**
+ * 
+ */
+package msb.javase.common;
+
+/**
+ * @author oradba
+ *
+ */
+public class TestDouble {
+
+  /**
+   * @param args
+   */
+  public static void main(String[] args) {
+    char[] cs = {'1', ',', '2', ';', '3', ',', '4', ',', '5', ';', '6', ',', '7', ',', '8'};
+    String s = new String(cs);
+    System.out.println(s);
+    
+    String[] s1= s.split(";");
+    double[][] doubleArray = new double[s1.length][];
+    for (int i = 0; i < s1.length; i++) {
+      String[] s2 = s1[i].split(",");
+      doubleArray[i] = new double[s2.length];
+      for (int j = 0; j < s2.length; j++) {
+        doubleArray[i][j] = Double.parseDouble(s2[j]);
+      }
+    }
+    
+    for (int i = 0; i < doubleArray.length; i++) {
+      for (int j = 0; j < doubleArray[i].length; j++) {
+        System.out.print(doubleArray[i][j] + " ");
+      }
+      System.out.println();
+    }
+  }
+
+}
