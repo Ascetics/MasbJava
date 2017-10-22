317e01a4b592ce77e90149e1a9f2765e59bdee9a
mingkun.ling <mingkun.ling@qq.com>
2017-10-22 23:22:54 +0800

    马士兵J2SE，第06章_常用类，11_递归列出目录结构.avi

---
+++ MasbJavaSE/src/msb/javase/common/FileList.java
@@ -0,0 +1,38 @@
+/**
+ * 
+ */
+package msb.javase.common;
+
+import java.io.File;
+
+/**
+ * @author mingkun.ling
+ *
+ */
+public class FileList {
+
+  /**
+   * @param args
+   */
+  public static void main(String[] args) {
+    String dir = new String("D:\\Ñ¸À×ÏÂÔØ");
+    File root = new File(dir);
+    tree(root, 0);
+  }
+  
+  private static void tree(File root, int level) {
+    StringBuilder spreBuilder = new StringBuilder();
+    for (int i = 0; i < level; i++) {
+      spreBuilder.append("  ");
+    }
+    System.out.println(spreBuilder + root.getName());
+    
+    if (root.isDirectory()) {
+      File[] files = root.listFiles();
+      for (int i = 0; i < files.length; i++) {
+        tree(files[i], level + 1);
+      }
+    }
+  }
+
+}
