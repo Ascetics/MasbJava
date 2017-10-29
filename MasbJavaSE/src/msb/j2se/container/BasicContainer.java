2aa22b6e8f28738ecb1605d2fd9bc638761e3872
mingkun.ling <mingkun.ling@qq.com>
2017-10-29 23:24:45 +0800

    J2SE\第07章_容器\08_Comparable.avi

--- MasbJavaSE/src/msb/j2se/container/BasicContainer.java
+++ MasbJavaSE/src/msb/j2se/container/BasicContainer.java
@@ -4,7 +4,10 @@
 package msb.j2se.container;
 
 import java.util.Collection;
+import java.util.Collections;
 import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.List;
 
 /**
  * @author oradba
@@ -16,7 +19,7 @@ public class BasicContainer {
    * @param args
    */
   public static void main(String[] args) {
-    Collection c = new HashSet();
+    Collection c = new LinkedList();
     c.add("hello");
     c.add(new Name("f1", "l1"));
     c.add(new Integer(100));
@@ -25,16 +28,26 @@ public class BasicContainer {
     System.out.println(c.remove(new Name("f1", "l1")));
     System.out.println(c);
     
+    List<Name> list = new LinkedList<Name>();
+    list.add(new Name("Karl", "M"));
+    list.add(new Name("Steve", "Lee"));
+    list.add(new Name("John", "O"));
+    list.add(new Name("Tom", "M"));
+    System.out.println(list);
+    Collections.sort(list);
+    System.out.println(list);
+
     Name n1 = new Name("f1", "l1");
     Name n2 = new Name("f1", "l2");
     System.out.println(n1.hashCode());
     System.out.println(n2.hashCode());
-    
+
   }
 
 }
 
-class Name {
+
+class Name implements Comparable<Name> {
   private String firstName, lastName;
 
   public Name(String firstName, String lastName) {
@@ -83,5 +96,14 @@ class Name {
     }
     return super.equals(obj);
   }
-  
+
+  @Override
+  public int compareTo(Name o) {
+    int lastResult = this.lastName.compareTo(o.lastName);
+    if (lastResult == 0) {
+      return this.firstName.compareTo(o.firstName);
+    }
+    return lastResult;
+  }
+
 }
